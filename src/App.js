94133ce118c907f918b4eed2af78b3b55a93c1ff
Richard Machado <mmachado.richard@gmail.com>
2024-06-14 20:24:09 -0700

    added a little styling and forced variable to switch signs if invalid inputs are attepted

--- src/App.js
+++ src/App.js
@@ -5,26 +5,41 @@ import "./styles.css";
 
 const Page = styled.div`
   box-sizing: border-box;
+  background-color:#b0bed6
 `;
 
-// const MainBox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   //border: 9px solid red;
-// `;
-
-const Label = styled.label`
-  //border: 3px solid green;
-  //width: 12;
-  font-size:18px;
- background-color: blue;
-`;
-const Heading = styled.h1`
+const Header = styled.h1`
    margin: auto;
   width: 50%;
  // border: 3px solid green;
   padding: 10px;
+  `
+
+
+const Label = styled.label`
+
+  font-size: 1.375rem;
+  font-weight: 600;
+  line-height: 1.25;
+
 `;
+
+const Invalid = styled.p`
+
+font-size: 1.375rem;
+font-weight: 600;
+line-height: 1.25;
+
+`
+const Responses = styled.p`
+
+font-size: 1.375rem;
+font-weight: 600;
+line-height: 1.25;
+
+`
+
+  
 export default  function App() {
 
   const [a, setNum1] = useState(1);
@@ -33,12 +48,17 @@ export default  function App() {
   const [plus, setPlus] = useState(0);
   const [minus, setMinus] = useState(0);
 
-
+  if ( a > 0 && c > 0 ) { 
+     setNum3(c *- 1);
+  }
+   if ( a < 0 && c < 0 ) { 
+     setNum3(c *- 1);
+  }
   const fourAC = 4 * a * c;
   const squareRoot = Math.sqrt((b * b) - fourAC);
   const plusTotal = ((b * -1) + squareRoot) / (2 * a);
   const minusTotal = ((b * -1) - squareRoot) / (2 * a);
-
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -47,40 +67,39 @@ export default  function App() {
   };
   return (
     <Page className="App-header">
-      <Heading> Testing : Enter quadratic equation Ax² + Bx + C</Heading>
+      <Header> Enter quadratic equation Ax² + Bx + C</Header>
       <form className="form" onSubmit={handleSubmit}>
         <Label>A</Label>
         <input
           // className="input"
           type="number"
           value={a}
-          onChange={(event) => setNum1(event.target.value)}
+          onChange={(e) => setNum1(e=e.target.value)}
         />
         <Label>B</Label>
         <input
           // className="input"
           type="number"
           value={b}
-          onChange={(event) => setNum2(event.target.value)}
+          onChange={(e) => setNum2(e.target.value)}
         />
         <Label>C</Label>
         <input
           //className="input"
           type="number"
           value={c}
-          onChange={(event) => setNum3(event.target.value)}
+          onChange={(e) => setNum3(e.target.value)}
         />
         <button className="button" type="submit">
           Get Zeroes
         </button>
         <b>
           {isNaN(plus) ? (
-            <p>Not a valid equation </p>
+            <Invalid>A and C must have opposite signs </Invalid>
           ) : (
             <>
-        
-              <div>x = {plus}</div>
-              <div>x = {minus}</div>
+              <Responses>x = {plus}</Responses>
+              <Responses>x = {minus}</Responses>
             </>
           )}
         </b>
