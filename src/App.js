7885e8d7909d77ac39af7242fd1740c4731028a9
Richard Machado <mmachado.richard@gmail.com>
2024-07-10 16:50:29 -0700

    corrected error that some functions can be all positive nums and still cross x axis

--- src/App.js
+++ src/App.js
@@ -5,61 +5,69 @@ import "./styles.css";
 
 const Page = styled.div`
   box-sizing: border-box;
-  background-color:#9fc5cc;
-  height:1000px;
+  background-color: #9fc5cc;
+  height: 1000px;
   //border:10px solid black;
 `;
 
 const Header = styled.h1`
-   margin: auto;
+  margin: auto;
   width: 50%;
   padding: 10px;
-  `
-
+`;
 
 const Label = styled.label`
-
   font-size: 1.375rem;
   font-weight: 600;
   line-height: 1.25;
-
 `;
 
 const Invalid = styled.p`
-
-font-size: 1.375rem;
-font-weight: 600;
-line-height: 1.25;
-
-`
+  font-size: 1.375rem;
+  font-weight: 600;
+  line-height: 1.25;
+`;
 const Responses = styled.p`
+  font-size: 1.375rem;
+  font-weight: 600;
+  line-height: 1.25;
+`;
 
-font-size: 1.375rem;
-font-weight: 600;
-line-height: 1.25;
-
-`
-
-  
-export default  function App() {
-
+export default function App() {
   const [a, setNum1] = useState(1);
   const [b, setNum2] = useState(1);
   const [c, setNum3] = useState(-56);
   const [plus, setPlus] = useState(0);
   const [minus, setMinus] = useState(0);
 
-  if ( a > 0 && c > 0 ) { 
-     setNum3(c *- 1);
-  }
-   if ( a < 0 && c < 0 ) { 
-     setNum3(c *- 1);
-  }
-  const fourAC = 4 * a * c;
-  const squareRoot = Math.sqrt((b * b) - fourAC);
-  const plusTotal = ((b * -1) + squareRoot) / (2 * a);
-  const minusTotal = ((b * -1) - squareRoot) / (2 * a);
+  // if (a > 0 && c > 0) {
+  //   setNum3(c * -1);
+  // }
+  // if (a < 0 && c < 0) {
+  //   setNum3(c * -1);
+  // }
+  // function MyComponent(props) {
+  // if (a === 1 ) {
  
+  //     <div>
+  //       <Header> Enter quadratic equation {a}x² + {b}x + {c}</Header>
+  //     </div>
+   
+  // } else {
+  //   return (
+  //     <div>
+  //       <h2>Please log in</h2>
+  //       <p>You need to be logged in to view this page.</p>
+  //     </div>
+  //   );
+  // }
+// }
+  
+  const fourAC = 4 * a * c;
+  const squareRoot = Math.sqrt(b * b - fourAC);
+  const plusTotal = (b * -1 + squareRoot) / (2 * a);
+  const minusTotal = (b * -1 - squareRoot) / (2 * a);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -68,14 +76,20 @@ export default  function App() {
   };
   return (
     <Page className="App-header">
-      <Header> Enter quadratic equation Ax² + Bx + C</Header>
+    
+      {/* <Header> Enter quadratic equation {a}x² + {b}x + {c}</Header> */}
+      <Header> {a===1 ? (
+     <h1>Enter quadratic equation x² + {b}x + {c}</h1>
+   ) : (
+     <h2>Enter quadratic equation {a}x² + {b}x + {c}</h2>
+   )}</Header>
       <form className="form" onSubmit={handleSubmit}>
         <Label>A</Label>
         <input
           // className="input"
           type="number"
           value={a}
-          onChange={(e) => setNum1(e=e.target.value)}
+          onChange={(e) => setNum1((e = e.target.value))}
         />
         <Label>B</Label>
         <input
@@ -96,7 +110,7 @@ export default  function App() {
         </button>
         <b>
           {isNaN(plus) ? (
-            <Invalid>A and C must have opposite signs </Invalid>
+            <Invalid> This equation doesn't have any roots </Invalid>
           ) : (
             <>
               <Responses>x = {plus}</Responses>
@@ -107,7 +121,4 @@ export default  function App() {
       </form>
     </Page>
   );
-  ;
-};
-
-
+}
